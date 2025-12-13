c269a85f5f73df1e31d8895b4d8ea781376da514
biyash-1 <sthaalish39@gmail.com>
2025-12-13 21:40:32 +0545

    ambulance

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 databaseConnection();
 
